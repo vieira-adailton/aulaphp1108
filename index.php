aa1a26dd475baa3a5750e0e8739e7109723970e3
unknown <lab1064@etecjk.local>
2025-08-11 19:41:33 -0300

    Aula PHP - Criação de variáveis, concatenação

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+$nome = "Adailton"; //Criação de variáveis em PHP - inicia com $
+$idade = 20; // Cria variável numerica
+$idadestr = "20"; //Cria uma variável string
+echo "Boa noite, ".$nome; // Concatena frase + variavel
+echo "<br> Idade: ".$idade;
+echo "<br> Idade: ".$idadestr;
